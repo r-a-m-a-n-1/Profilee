692813189b0fa38d7d0e89b74b014939ac75ef10
r-a-m-a-n-1 <159043051+r-a-m-a-n-1@users.noreply.github.com>
2024-03-15 21:31:19 +0530

    Add files via upload

---
+++ Material/project51.js
@@ -0,0 +1,12 @@
+const tipcal=()=>{
+    let amount=document.getElementById(`bill_amount`).value;
+    let perc=document.getElementById(`tip_perc`).value;
+
+    let tip=amount *(perc/100);
+    let total=tip + Number(amount);
+
+    document.getElementById(`tip_amount`).value=tip;
+    document.getElementById(`total_billed`).value=total;
+
+
+};
